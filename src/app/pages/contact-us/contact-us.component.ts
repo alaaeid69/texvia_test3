a2e7d1a1a15c81222e9e56ed5a72b07b0bc27797
alaaeid69 <alaaelsharkawy69@gmail.com>
2025-05-25 17:18:30 +0300

    contact page update

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-contact-us',
   imports: [],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
-export class ContactUsComponent implements OnInit {
-  
-  ngOnInit(): void {
-    console.log('hey')
-  }
+export class ContactUsComponent{
 
 }
