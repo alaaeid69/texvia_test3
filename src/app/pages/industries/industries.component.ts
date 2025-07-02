371a267fee6709e96ceb66fa7558d4aec49ebe30
alaaeid69 <alaaelsharkawy69@gmail.com>
2025-07-02 14:15:28 +0300

    update insights and industry link

--- src/app/pages/industries/industries.component.ts
+++ src/app/pages/industries/industries.component.ts
@@ -1,17 +1,62 @@
-import { Component} from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { fromEvent, Subject, takeUntil, throttleTime } from 'rxjs';
 @Component({
   selector: 'app-industries',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './industries.component.html',
   styleUrl: './industries.component.scss'
 })
 export class IndustriesComponent{
 
-scrollToSection(sectionId: string){
-const element = document.getElementById(sectionId);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' , block:'start'})
-}
+// scrollToSection(sectionId: string){
+// const element = document.getElementById(sectionId);
+//     if (element) {
+//       element.scrollIntoView({ behavior: 'smooth' , block:'start'})
+// }
 
-}
+// }
+ private readonly router = inject(Router);
+
+private destroy$ = new Subject<void>(); 
+  constructor() {
+    fromEvent(window, 'scroll')
+      .pipe(
+        throttleTime(100, undefined, { leading: true, trailing: true }), 
+        takeUntil(this.destroy$) 
+      )
+      .subscribe((event) => this.onWindowScroll(event));
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onWindowScroll(event: Event): void {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop; 
+    const adjustedScrollPosition = scrollPosition + 400
+
+    const sections = document.querySelectorAll<HTMLElement>('.industry-cards')
+    let activeSectionId: string | null = null
+    sections.forEach((section: HTMLElement) => {
+      const sectionTop = section.offsetTop;
+      const sectionBottom = sectionTop + section.offsetHeight;
+
+      if (
+        sectionTop <= adjustedScrollPosition &&
+        sectionBottom > adjustedScrollPosition
+      ) {
+        activeSectionId = section.id; 
+      }
+    });
+
+       const navLinks = document.querySelectorAll<HTMLAnchorElement>('.nav .nav-item .nav-link');
+    navLinks.forEach((link: HTMLAnchorElement) => {
+      if (activeSectionId && link.href.includes(activeSectionId)) {
+        link.classList.add('active')
+      } else {
+        link.classList.remove('active')
+      }
+    });
+  }
 }
